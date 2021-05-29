fd04069f52e11193338b9215ab5c42cb9736d8ed
Eric Carmona <ericscarmona@yahoo.com>
2021-05-29 09:55:46 -0700

    functioning controlla/service

--- apps/evo-tree-api/src/app/species/species.controller.ts
+++ apps/evo-tree-api/src/app/species/species.controller.ts
@@ -1,6 +1,24 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('/api/species')
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { SpeciesService } from './species.service';
+import { Species } from './species.entity';
+@Controller('species')
 export class SpeciesController {
-  constructor() {}
+  constructor(private _speciesService: SpeciesService) {}
+
+  @Get()
+  async getAllSpecies(): Promise<Species[]> {
+    return await this._speciesService.getAllSpecies();
+  }
+
+  @Get('/:id')
+  getSpeciesById() {}
+
+  @Post()
+  createNewSpecies() {}
+
+  @Put('/:id')
+  updateSpecies() {}
+
+  @Delete('/:id')
+  deleteSpeciesById() {}
 }
--- apps/evo-tree-api/src/app/species/species.service.ts
+++ apps/evo-tree-api/src/app/species/species.service.ts
@@ -5,5 +5,28 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class SpeciesService {
-  constructor(@InjectRepository(Species) speciesRepo: Repository<Species>) {}
+  constructor(
+    @InjectRepository(Species) private _speciesRepo: Repository<Species>
+  ) {
+    this.seedRepo();
+    // this._speciesRepo.clear();
+  }
+
+  async seedRepo() {
+    if (await this.isRepoEmpty()) {
+      await this._speciesRepo.insert({
+        name: 'Amoeba',
+        parentId: null,
+        children: null,
+      });
+    }
+  }
+
+  async isRepoEmpty(): Promise<boolean> {
+    return (await this._speciesRepo.count()) === 0;
+  }
+
+  async getAllSpecies() {
+    return await this._speciesRepo.find();
+  }
 }
